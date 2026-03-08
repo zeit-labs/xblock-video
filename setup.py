31ed608b1d945ef9d3f2cb1ca481adb169774a85
Shadi Naif <shadinaif@gmail.com>
2026-03-08 15:54:00 +0300

    fix: package requirements in setup.py

--- setup.py
+++ setup.py
@@ -48,10 +48,8 @@ setup(
         'video_xblock',
     ],
     install_requires=[
-        'pycaption==2.1.0',
-        'requests>=2.9.1,<3.0.0',
+        'pycaption>=2.1.0,<3.0.0',
         'babelfish>=0.6.0',
-        'Xblock>=4.0.1,<5.0.0',
     ],
     entry_points={
         'xblock.v1': [
